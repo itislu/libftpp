952c0beee0187eb55cd8afe5dccc8b24e4eed6b1
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-16 18:29:26 +0100

    style: Change clang-format to never indent namespaces
    If there are multiple namespaces in the same file and not only one of them has implementations, the namespaces should probably be separated into separate files.

--- utils/log.hpp
+++ utils/log.hpp
@@ -8,43 +8,43 @@
 namespace utils {
 namespace log {
 
-	const size_t label_width_text = 8;
-	const size_t label_width_colored = 25;
+const size_t label_width_text = 8;
+const size_t label_width_colored = 25;
 
-	template <typename T>
-	std::string error(const T& msg)
-	{
-		std::ostringstream os;
-		os << std::left << std::setw(label_width_colored)
-		   << BOLD(RED("[ERROR]")) << msg;
-		return os.str();
-	}
+template <typename T>
+std::string error(const T& msg)
+{
+	std::ostringstream os;
+	os << std::left << std::setw(label_width_colored) << BOLD(RED("[ERROR]"))
+	   << msg;
+	return os.str();
+}
 
-	template <typename T>
-	std::string info(const T& msg)
-	{
-		std::ostringstream os;
-		os << std::left << std::setw(label_width_colored)
-		   << BOLD(BLUE("[INFO]")) << msg;
-		return os.str();
-	}
+template <typename T>
+std::string info(const T& msg)
+{
+	std::ostringstream os;
+	os << std::left << std::setw(label_width_colored) << BOLD(BLUE("[INFO]"))
+	   << msg;
+	return os.str();
+}
 
-	template <typename T>
-	std::string ok(const T& msg)
-	{
-		std::ostringstream os;
-		os << std::left << std::setw(label_width_colored) << BOLD(GREEN("[OK]"))
-		   << msg;
-		return os.str();
-	}
+template <typename T>
+std::string ok(const T& msg)
+{
+	std::ostringstream os;
+	os << std::left << std::setw(label_width_colored) << BOLD(GREEN("[OK]"))
+	   << msg;
+	return os.str();
+}
 
-	template <typename T>
-	std::string line(const T& msg)
-	{
-		std::ostringstream os;
-		os << '\n' << std::left << std::setw(label_width_text) << "" << msg;
-		return os.str();
-	}
+template <typename T>
+std::string line(const T& msg)
+{
+	std::ostringstream os;
+	os << '\n' << std::left << std::setw(label_width_text) << "" << msg;
+	return os.str();
+}
 
 } // namespace log
 } // namespace utils
--- utils/string.hpp
+++ utils/string.hpp
@@ -7,30 +7,30 @@
 namespace utils {
 namespace string {
 
-	template <typename T>
-	std::string to_string(T v)
-	{
-		std::ostringstream oss;
-		oss << v;
-		return oss.str();
-	}
+template <typename T>
+std::string to_string(T v)
+{
+	std::ostringstream oss;
+	oss << v;
+	return oss.str();
+}
 
-	template <typename T>
-	std::string to_string(T* v)
-	{
-		if (v == NULL) {
-			return "(null)";
-		}
-		std::ostringstream oss;
-		oss << v;
-		return oss.str();
+template <typename T>
+std::string to_string(T* v)
+{
+	if (v == NULL) {
+		return "(null)";
 	}
+	std::ostringstream oss;
+	oss << v;
+	return oss.str();
+}
 
-	template <>
-	inline std::string to_string(bool v)
-	{
-		return v ? "true" : "false";
-	}
+template <>
+inline std::string to_string(bool v)
+{
+	return v ? "true" : "false";
+}
 
 } // namespace string
 } // namespace utils
