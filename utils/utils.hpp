5e202c7e178f1aed32a19b01ff85804869eba163
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-16 20:23:27 +0100

    feat: Add custom `rand()` function to utils

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <fstream>
+#include <ios>
 #include <string>
 
 #define BOLD(str)  std::string() + "\033[1m" + str + "\033[0m"
@@ -22,5 +24,18 @@ void swap(T& a, T& b)
 	a = b;
 	b = tmp;
 }
+
+// NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
+template <typename T>
+void rand(T* v, std::streamsize size)
+{
+	std::ifstream urandom;
+
+	urandom.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	urandom.open("/dev/urandom", std::ios::binary);
+	urandom.read(reinterpret_cast<char*>(v), size);
+	urandom.close();
+}
+// NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
 
 } // namespace utils
