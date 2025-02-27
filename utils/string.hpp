5e3bba50f9a7728dfa30d1b264b45b55e8afb026
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-02-27 23:21:27 +0100

    feat: Add `from_string()` and `transform` to string utils

--- utils/string.hpp
+++ utils/string.hpp
@@ -1,14 +1,40 @@
 #pragma once
 
 #include <cstddef>
+#include <iostream>
 #include <sstream>
+#include <stdexcept>
 #include <string>
+#include <typeinfo>
 
 namespace utils {
 namespace string {
 
 std::string& trim(std::string& str);
 std::string trim(const std::string& str);
+
+template <typename T>
+T from_string(const std::string& str)
+{
+	T res;
+	if (!(std::istringstream(str) >> res)) {
+		throw std::invalid_argument(std::string("Cannot convert to ")
+		                            + typeid(T).name() + ": " + str);
+	}
+	return res;
+}
+
+template <>
+inline bool from_string<bool>(const std::string& str)
+{
+	bool b = false;
+	if (!(std::istringstream(str) >> std::boolalpha >> b)
+	    && !(std::istringstream(str) >> b)) {
+		throw std::invalid_argument(std::string("Cannot convert to ")
+		                            + typeid(bool).name() + ": " + str);
+	}
+	return b;
+}
 
 template <typename T>
 std::string to_string(T v)
@@ -33,6 +59,22 @@ template <>
 inline std::string to_string(bool v)
 {
 	return v ? "true" : "false";
+}
+
+template <typename InputIterator,
+          typename OutputIterator,
+          typename UnaryOperator>
+OutputIterator transform(InputIterator first,
+                         InputIterator last,
+                         OutputIterator result,
+                         UnaryOperator op)
+{
+	while (first != last) {
+		*result = op(*first);
+		++result;
+		++first;
+	}
+	return result;
 }
 
 } // namespace string
