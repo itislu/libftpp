ff5832eaf68bb9aefe44a06cb4c46155ee5ad607
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-03-15 06:54:51 +0100

    feat: Add type traits header with class method detection and `enable_if` replicate
    Learnings: Metaprogramming, SFINAE

---
+++ utils/type_traits.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace utils {
+
+template <bool B, typename T = void>
+struct enable_if {};
+
+template <typename T>
+struct enable_if<true, T> {
+	typedef T type;
+};
+
+/**
+ * Generates type traits that check if a class has a specific method.
+ * The generated type traits are named has_<method_name>.
+ */
+#define HAS_METHOD(name, signature)                   \
+	template <typename T>                             \
+	struct has_##name {                               \
+	private:                                          \
+		typedef char yes[1];                          \
+		typedef char no[2];                           \
+                                                      \
+		template <typename U, signature>              \
+		struct SFINAE {};                             \
+                                                      \
+		template <typename U>                         \
+		static yes& test(SFINAE<U, &U::name>*);       \
+		template <typename U>                         \
+		static no& test(...);                         \
+                                                      \
+	public:                                           \
+		enum {                                        \
+			value = sizeof(test<T>(0)) == sizeof(yes) \
+		};                                            \
+	};
+
+#undef HAS_METHOD
+
+} // namespace utils
