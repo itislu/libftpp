cc5897ac10a03e9adca8cf695c626b20e6cfd7fa
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-04-07 19:59:24 +0200

    style: Make `HAS_METHOD` more readable

--- lib/libftpp/type_traits.hpp
+++ lib/libftpp/type_traits.hpp
@@ -19,28 +19,29 @@ struct enable_if<true, T> {
  * Generates type traits that check if a class has a specific method.
  * The generated type traits are named has_<method_name>.
  */
-#define HAS_METHOD(NAME, SIGNATURE)                      \
-	template <typename T>                                \
-	struct has_##NAME {                                  \
-	private:                                             \
-		template <typename U, SIGNATURE>                 \
-		struct Sfinae {};                                \
-                                                         \
-		typedef char yes[1];                             \
-		typedef char no[2];                              \
-                                                         \
-		template <typename U>                            \
-		static yes& test(Sfinae<U, &U::NAME>*);          \
-		template <typename U>                            \
-		static no& test(...);                            \
-                                                         \
-	public:                                              \
-		enum {                                           \
-			value = sizeof(test<T>(NULL)) == sizeof(yes) \
-		};                                               \
+#define HAS_METHOD(RETURN_TYPE, METHOD_NAME, ARGS)                           \
+	template <typename T>                                                    \
+	struct has_##METHOD_NAME {                                               \
+	private:                                                                 \
+		/* 2nd param: member-function pointer non-type template parameter */ \
+		template <typename ToCheck, RETURN_TYPE (ToCheck::*)(ARGS)>          \
+		struct Sfinae {};                                                    \
+                                                                             \
+		typedef char yes[1];                                                 \
+		typedef char no[2];                                                  \
+                                                                             \
+		template <typename ToCheck>                                          \
+		static yes& test(Sfinae<ToCheck, &ToCheck::METHOD_NAME>*);           \
+		template <typename>                                                  \
+		static no& test(...);                                                \
+                                                                             \
+	public:                                                                  \
+		enum {                                                               \
+			value = sizeof(test<T>(NULL)) == sizeof(yes)                     \
+		};                                                                   \
 	};
 
-HAS_METHOD(swap, void (U::*)(U&))
+HAS_METHOD(void, swap, ToCheck&)
 
 #undef HAS_METHOD
 
