19eb42d5474092ee5edfac90326cd708c42179c9
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-04-07 19:21:00 +0200

    feat: Add `Comparison` in `operators` namespace to generate comparison operators

---
+++ lib/libftpp/operators.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+namespace ft {
+namespace operators {
+
+/**
+ * @brief Struct to inherit from to generate all remaining comparison operators
+ * for the inheriting class.
+ *
+ * The `operator<` has to be defined for the inheriting class.
+ * To generate operators which compare with a different type, the `operater<`
+ * has to be defined outside of the class, with that type once on the rhs and
+ * once on the lhs.
+ *
+ * Usage:
+ * 1. Implement `operator<` for your class.
+ * 2. Inherit from this struct.
+ *
+ * This works because `Comparison` acts like a tag, and by inheriting from it it
+ * gives the inheriting class access to the ft::operators namespace, which
+ * contains the function templates for the operators.
+ */
+struct Comparison {};
+
+template <typename T, typename U>
+bool operator==(const T& lhs, const U& rhs);
+
+template <typename T, typename U>
+bool operator!=(const T& lhs, const U& rhs);
+
+template <typename T, typename U>
+bool operator>(const T& lhs, const U& rhs);
+
+template <typename T, typename U>
+bool operator<=(const T& lhs, const U& rhs);
+
+template <typename T, typename U>
+bool operator>=(const T& lhs, const U& rhs);
+
+} // namespace operators
+} // namespace ft
+
+#include "src/operators.tpp" // IWYU pragma: export
---
+++ lib/libftpp/src/operators.tpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../operators.hpp"
+
+namespace ft {
+namespace operators {
+
+template <typename T, typename U>
+bool operator==(const T& lhs, const U& rhs)
+{
+	return !(lhs < rhs) && !(rhs < lhs);
+}
+
+template <typename T, typename U>
+bool operator!=(const T& lhs, const U& rhs)
+{
+	return !(lhs == rhs);
+}
+
+template <typename T, typename U>
+bool operator>(const T& lhs, const U& rhs)
+{
+	return rhs < lhs;
+}
+
+template <typename T, typename U>
+bool operator<=(const T& lhs, const U& rhs)
+{
+	return !(lhs > rhs);
+}
+
+template <typename T, typename U>
+bool operator>=(const T& lhs, const U& rhs)
+{
+	return !(lhs < rhs);
+}
+
+} // namespace operators
+} // namespace ft
