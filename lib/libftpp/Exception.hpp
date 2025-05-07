ca6766ddb2a216a1a0b520a2ef19b10e8a75ec46
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2025-05-07 13:59:31 +0200

    refactor!: Make `Exception` nothrow copy constructable + reorder constructor args

--- lib/libftpp/Exception.hpp
+++ lib/libftpp/Exception.hpp
@@ -5,38 +5,35 @@
 
 namespace ft {
 
-/**
- * Important: Always catch by reference!
- * Catching by value risks a call to std::terminate because this class contains
- * std::strings which could throw bad_alloc when copy constructed.
- */
 class Exception : public std::exception {
 public:
 	explicit Exception(const std::string& msg,
-	                   const std::string& where = "",
-	                   const std::string& who = "");
-	Exception(const Exception& other);
+	                   const std::string& who = "",
+	                   const std::string& where = "");
+	Exception(const Exception& other) throw();
 	virtual ~Exception() throw();
 	Exception& operator=(Exception other) throw();
 
 	const char* what() const throw();
 	void swap(Exception& other) throw();
 
-	Exception& set_where(const std::string& where);
 	Exception& set_who(const std::string& who);
+	Exception& set_where(const std::string& where);
 	const std::string& msg() const throw();
-	const std::string& where() const throw();
 	const std::string& who() const throw();
+	const std::string& where() const throw();
 
 private:
-	void _update_full_msg();
+	Exception();
+
+	void _update_what_output();
 
 	std::string _msg;
-	std::string _where;
 	std::string _who;
-	std::string _full_msg;
+	std::string _where;
+	std::string _what_output;
 };
 
-void swap(Exception& lhs, Exception& rhs);
+void swap(Exception& lhs, Exception& rhs) throw();
 
 } // namespace ft
--- lib/libftpp/src/Exception/Exception.cpp
+++ lib/libftpp/src/Exception/Exception.cpp
@@ -5,21 +5,42 @@
 namespace ft {
 
 Exception::Exception(const std::string& msg,
-                     const std::string& where /*= ""*/,
-                     const std::string& who /*= ""*/)
+                     const std::string& who /*= ""*/,
+                     const std::string& where /*= ""*/)
     : _msg(msg),
-      _where(where),
-      _who(who)
+      _who(who),
+      _where(where)
 {
-	_update_full_msg();
+	_update_what_output();
 }
 
-Exception::Exception(const Exception& other)
-    : _msg(other._msg),
-      _where(other._where),
-      _who(other._who),
-      _full_msg(other._full_msg)
-{}
+Exception::Exception(const Exception& other) throw()
+{
+	try {
+		_msg = other._msg;
+	}
+	catch (...) {
+		// REASON: Exception objects should be nothrow constructable
+	}
+	try {
+		_who = other._who;
+	}
+	catch (...) {
+		// REASON: Exception objects should be nothrow constructable
+	}
+	try {
+		_where = other._where;
+	}
+	catch (...) {
+		// REASON: Exception objects should be nothrow constructable
+	}
+	try {
+		_what_output = other._what_output;
+	}
+	catch (...) {
+		// REASON: Exception objects should be nothrow constructable
+	}
+}
 
 Exception::~Exception() throw() {}
 
@@ -29,42 +50,42 @@ Exception& Exception::operator=(Exception other) throw()
 	return *this;
 }
 
-const char* Exception::what() const throw() { return _full_msg.c_str(); }
+const char* Exception::what() const throw() { return _what_output.c_str(); }
 
 void Exception::swap(Exception& other) throw()
 {
 	_msg.swap(other._msg);
-	_where.swap(other._where);
 	_who.swap(other._who);
-	_full_msg.swap(other._full_msg);
-}
-
-Exception& Exception::set_where(const std::string& where)
-{
-	_where = where;
-	_update_full_msg();
-	return *this;
+	_where.swap(other._where);
+	_what_output.swap(other._what_output);
 }
 
 Exception& Exception::set_who(const std::string& who)
 {
 	_who = who;
-	_update_full_msg();
+	_update_what_output();
+	return *this;
+}
+
+Exception& Exception::set_where(const std::string& where)
+{
+	_where = where;
+	_update_what_output();
 	return *this;
 }
 
 const std::string& Exception::msg() const throw() { return _msg; }
 
-const std::string& Exception::where() const throw() { return _where; }
-
 const std::string& Exception::who() const throw() { return _who; }
 
-void Exception::_update_full_msg()
+const std::string& Exception::where() const throw() { return _where; }
+
+void Exception::_update_what_output()
 {
-	_full_msg = (_where.empty() ? "" : GRAY(_where + ": "))
-	            + (_who.empty() ? "" : _who + ": ") + _msg;
+	_what_output = (_where.empty() ? "" : GRAY(_where + ": "))
+	               + (_who.empty() ? "" : _who + ": ") + _msg;
 }
 
-void swap(Exception& lhs, Exception& rhs) { lhs.swap(rhs); }
+void swap(Exception& lhs, Exception& rhs) throw() { lhs.swap(rhs); }
 
 } // namespace ft
