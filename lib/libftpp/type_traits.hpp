efa6c60a90c973fa1dfd9143dce96cd3c2ed2424
Lukas Dullinger <itislu.git@gmail.com>
2025-08-22 18:30:50 +0200

    fix: Prevent `HAS_MEMBER_FUNCTION` from declaring reserved name

--- lib/libftpp/type_traits.hpp
+++ lib/libftpp/type_traits.hpp
@@ -347,26 +347,26 @@ struct can_be_return_type;
  * Usage example: `HAS_MEMBER_FUNCTION(void, swap, (T&))`
  */
 // NOLINTBEGIN(bugprone-macro-parentheses)
-#define HAS_MEMBER_FUNCTION(RETURN_TYPE, NAME, ARGS_IN_PARENS)                 \
-	namespace _has_member_function_##NAME                                      \
-	{                                                                          \
-                                                                               \
-		/* 2nd param: member-function pointer non-type template parameter */   \
-		template <typename T, RETURN_TYPE(T::*) ARGS_IN_PARENS>                \
-		struct Sfinae {};                                                      \
-                                                                               \
-		template <typename T, typename = void>                                 \
-		struct Impl : ft::false_type {};                                       \
-                                                                               \
-		template <typename T>                                                  \
-		struct Impl<T, typename ft::voider<Sfinae<T, &T::NAME> >::type>        \
-		    : ft::true_type {};                                                \
-                                                                               \
-	} /* namespace _has_member_function_##NAME */                              \
-                                                                               \
-	template <typename T>                                                      \
-	struct has_member_function_##NAME : _has_member_function_##NAME::Impl<T> { \
-	};
+#define HAS_MEMBER_FUNCTION(RETURN_TYPE, NAME, ARGS_IN_PARENS)               \
+	namespace detail_has_member_function_##NAME                              \
+	{                                                                        \
+                                                                             \
+		/* 2nd param: member-function pointer non-type template parameter */ \
+		template <typename T, RETURN_TYPE(T::*) ARGS_IN_PARENS>              \
+		struct Sfinae {};                                                    \
+                                                                             \
+		template <typename T, typename = void>                               \
+		struct Impl : ft::false_type {};                                     \
+                                                                             \
+		template <typename T>                                                \
+		struct Impl<T, typename ft::voider<Sfinae<T, &T::NAME> >::type>      \
+		    : ft::true_type {};                                              \
+                                                                             \
+	} /* namespace detail_has_member_function_##NAME */                      \
+                                                                             \
+	template <typename T>                                                    \
+	struct has_member_function_##NAME                                        \
+	    : detail_has_member_function_##NAME::Impl<T> {};
 // NOLINTEND(bugprone-macro-parentheses)
 
 /* has_member_function_swap */
