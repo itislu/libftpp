3f00cdb56f693972330f269b268c18205cfbbb25
Lukas Dullinger <itislu.git@gmail.com>
2025-10-21 11:49:41 +0200

    refactor(`utility`): Use `unique_ptr` in `demangle`

--- libftpp/utility/demangle.cpp
+++ libftpp/utility/demangle.cpp
@@ -1,5 +1,5 @@
+#include "libftpp/memory.hpp"
 #include "libftpp/utility.hpp"
-#include <exception>
 #include <string>
 
 #if defined(__GNUC__) || defined(__clang__)
@@ -13,20 +13,11 @@ namespace ft {
 std::string demangle(const char* mangled_name)
 {
 #ifdef LIBFTPP_HAVE_CXXABI_H
-	std::string result;
 	int status = 0;
-	char* const demangled =
-	    abi::__cxa_demangle(mangled_name, FT_NULLPTR, FT_NULLPTR, &status);
-
-	try {
-		result = status == 0 ? demangled : mangled_name;
-	}
-	catch (const std::exception& e) {
-		std::free(demangled); // NOLINT(cppcoreguidelines-no-malloc)
-		throw;
-	}
-	std::free(demangled); // NOLINT(cppcoreguidelines-no-malloc)
-	return result;
+	const ft::unique_ptr<char, void (*)(void*)> demangled(
+	    abi::__cxa_demangle(mangled_name, FT_NULLPTR, FT_NULLPTR, &status),
+	    std::free);
+	return status == 0 ? demangled.get() : mangled_name;
 #else  // LIBFTPP_HAVE_CXXABI_H
 	return mangled_name;
 #endif // LIBFTPP_HAVE_CXXABI_H
