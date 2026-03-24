7da8e900cfb8ab563f6cc6baad71341dbd9a0153
Lukas Dullinger <itislu.git@gmail.com>
2026-03-24 05:20:22 +0100

    perf(`numeric`): Simplify `abs_diff()`
    Not sure why I did it this long way before.
    The new way is certainly safe because of unsigned mod arithmetic.

--- libftpp/numeric/abs_diff.cpp
+++ libftpp/numeric/abs_diff.cpp
@@ -37,20 +37,8 @@ namespace _abs_diff {
 template <typename U, typename I>
 static U abs_diff(I a, I b) throw()
 {
-	// Same sign - subtract higher from lower.
-	if ((a >= 0 && b >= 0) || (a < 0 && b < 0)) {
-		return a > b ? a - b : b - a;
-	}
-	/*
-	0. Higher nbr is known to be positive or 0, lower negative.
-	1. Cast known positive nbr to unsigned to make all additions to the total
-	   unsigned.
-	2. Add 1 to negative nbr (losing 1 diff) to avoid overflow in next step.
-	3. Make result of previous step positive and unsigned-add to total.
-	4. Add back the lost diff to total.
-	*/
-	return a > b ? static_cast<U>(a) + -(b + 1) + 1
-	             : static_cast<U>(b) + -(a + 1) + 1;
+	return a > b ? static_cast<U>(a) - static_cast<U>(b)
+	             : static_cast<U>(b) - static_cast<U>(a);
 }
 
 } // namespace _abs_diff
